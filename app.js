ecc2c71c825796e8663601de61b52fd38f0ffcad
emagrorrom <373407641@qq.com>
2016-03-15 14:01:52 +0800

    project dir init

--- app.js
+++ app.js
@@ -2,15 +2,17 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let path = require('path');
+let api = require('./api');
 
 let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', api);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendfile('./public/html/calculator.html');
 });
 
 app.listen(8080, () => {
